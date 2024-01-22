f63415d5fa165743aaee1258e56c3527e4241c5b
EgorVjalkov <e.vjalkov@yandex.ru>
2024-01-22 11:24:29 +0300

    добавил работу с превышением лимита, что очень важно в работе бота. нужно рихтовать и дрделывать с другими функциями

--- chronossus/storage.py
+++ chronossus/storage.py
@@ -1,30 +1,22 @@
 from random import randint, choice
 
 
-def if_limit_decorator(func):
-    def wrapper(*args, **kwargs):
-        result: TokenStorage = func(*args, **kwargs)
-        if result.token_num <= result.limit:
-            return result
-        else:
-            print(f'стоп-слово: {result.if_limit}')
-        return wrapper()
-
-
 class TokenStorage:
     def __init__(self,
                  storage_name: str,
-                 tokens: int | dict,
-                 limit: int,
+                 default_tokens: int | dict,
+                 default_token_num: int,
+                 limit: list,
                  if_limit: str = 'stop',
                  die: list = None):
 
         self.token_name = storage_name
-        self.tokens = tokens
+        self.default_tokens_num = default_token_num
         self.limit = limit
         self.die = die
         self.if_limit = if_limit
 
+        self.tokens = default_tokens
         self.token_pool = []
 
     def __repr__(self):
@@ -33,11 +25,6 @@ class TokenStorage:
         else:
             return f'TokenStorage "{self.token_name}": {self.tokens}'
 
-    def get_token_pool(self) -> list:
-        for token_type in self.tokens:
-            self.token_pool += [token_type] * self.tokens[token_type]
-        return self.token_pool
-
     @property
     def token_num(self):
         if self.token_pool:
@@ -45,7 +32,15 @@ class TokenStorage:
         else:
             return self.tokens
 
-    @if_limit_decorator
+    def is_overage(self, result):
+        token_num = list(range(*self.limit))
+        return result not in token_num
+
+    def get_token_pool(self) -> list:
+        for token_type in self.tokens:
+            self.token_pool += [token_type] * self.tokens[token_type]
+        return self.token_pool
+
     def draw_from_pool(self, tokens: int) -> list:
         drawn = []
         for num in range(tokens):
@@ -54,36 +49,61 @@ class TokenStorage:
             drawn.append(token)
         return drawn
 
-    @if_limit_decorator
+    def add_tokens_by_die(self,
+                          rolls: int = 1) -> int | str:
+        for num in range(rolls):
+            add_tokens = choice(self.die)
+            print(add_tokens)
+            if self.is_overage(self.token_num+add_tokens):
+                return self.if_limit
+            else:
+                self.tokens += add_tokens
+        return self.tokens
+
+    def add_tokens_by_num(self,
+                          token_num: int,
+                          token_list: list = None):
+
+        for token in range(token_num):
+            if self.is_overage(self.token_num+1):
+                return self.if_limit
+            else:
+                if token_list:
+                    self.token_pool.append(token_list.pop(token_list.index(token)))
+                else:
+                    self.tokens += 1
+        return self
+
     def add_(self,
              mod: str = 'simple',
-             tokens: int | list = None,
+             token_num: int = None,
+             token_list: list = None,
              rolls: int = None) -> object:
 
         if mod == 'by_die':
-            for num in range(rolls):
-                self.tokens += choice(self.die)
-                print(self.token_num)
+            return self.add_tokens_by_die(rolls)
         else:
-            if isinstance(tokens, list):
-                self.token_pool.extend(tokens)
-            else:
-                self.tokens += tokens
-        return self
+            return self.add_tokens_by_num(token_num, token_list)
 
-    @if_limit_decorator
     def spend_(self, tokens: int | list):
         if self.token_pool:
             for token in tokens:
                 self.token_pool.remove(token)
+            return self.token_pool
         else:
             self.tokens -= tokens
-        return self
+            return self.tokens
 
 
-#ts = TokenStorage('EnergyPool', {'ch': 5, 'ex': 5}, 0)
-#ts.get_token_pool()
-#print(ts)
+ts = TokenStorage('EnergyPool',
+                  {'ch': 5, 'ex': 5},
+                  10,
+                  limit=[0, 20],
+                  if_limit='stop')
+ts.get_token_pool()
+print(ts)
+# автоматизируй работу с жетонами. сделай функции спенд и адд в одной, возиожно так можно.
+# нужно сделать токенс универвльной переменной чтоб не париться с токенс пулом
 #drawn_t = ts.draw_from_pool(tokens=3)
 #print(drawn_t)
 #print(ts)
@@ -99,12 +119,14 @@ class TokenStorage:
 
 paradox = TokenStorage('ParadoxStorage',
                        0,
-                       limit=3,
+                       0,
+                       limit=[0, 3],
                        if_limit='anomaly',
                        die=[0, 1, 1, 1, 1, 2])
 print(paradox)
-print(type(paradox))
-paradox.add_(mod='by_die', rolls=3)
+overage = paradox.add_(token_num=3)
+#overage = paradox.add_(mod='by_die', rolls=3)
 print(paradox)
-paradox.spend_(1)
-print(paradox)
+print(overage)
+#paradox.spend_(1)
+#print(paradox)
